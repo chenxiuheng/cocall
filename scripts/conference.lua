25b56fdddb0d5b6825f4960ba861a018488de8f0
xiuheng chen <chenxiuheng@gmail.com>
2015-07-03 09:58:58 +0800

    如果会议室无效，或者不在会议室中，则不能加入会议室

--- scripts/conference.lua
+++ scripts/conference.lua
@@ -9,40 +9,38 @@ local conf_num = 0;
 caller_id =  session:getVariable("caller_id_number");
 conf_num  = session:getVariable("destination_number");
 
-function session_hangup_hook()
-  freeswitch.consoleLog("NOTICE", "Session hangup: \n")
-end
-
 session:answer();
-session:setHangupHook("session_hangup_hook")
-
 
 local foundIt = false;
-local service = newConferenceService(conf_num);
-local members = service.getMembers(caller_id);
-for i, member in ipairs(members)  do
+local conf = getConferenceInfo(conf_num);
+if nil ~= conf and '1' == conf['valid'] then
+    local service = newConferenceService(conf_num);
+    local members = service.getMembers(caller_id);
     service.getMemberStates(caller_id);
 
-    --  wait user audio+video send to FS
-    --     if don't wait, FS will has problem(s) when punch NAT
-    freeswitch.consoleLog("info", string.format("user %s waiting to join conference %s\n", caller_id, conf_num));
-    freeswitch.msleep(2 * 1000); -- because default RTP timeout is 1800 
+    for i, member in ipairs(members)  do
+        --  wait user audio+video send to FS
+        --     if don't wait, FS will has problem(s) when punch NAT
+        freeswitch.consoleLog("info", string.format("user %s waiting to join conference %s\n", caller_id, conf_num));
+        freeswitch.msleep(2 * 1000); -- because default RTP timeout is 1800 
 
 
-    -- if session is NOT disavailable
-    if session:ready() then
-        foundIt = true;
-        local conf_name = string.format("%s@ultrawideband", conf_num, caller_id);
-        session:execute("conference", conf_name)
+        -- if session is NOT disavailable
+        if session:ready() then
+            foundIt = true;
+            local conf_name = string.format("%s@ultrawideband", conf_num, caller_id);
+            session:execute("conference", conf_name)
 
-        freeswitch.consoleLog("notice", string.format("user %s joined conference %s\n", caller_id, conf_num));
-    else
-        freeswitch.consoleLog("warning", string.format("session NOT ready, user %s canceled join conference %s \n", caller_id, conf_num));
+            freeswitch.consoleLog("notice", string.format("user %s joined conference %s\n", caller_id, conf_num));
+        else
+            freeswitch.consoleLog("warning", string.format("session NOT ready, user %s canceled join conference %s \n", caller_id, conf_num));
+        end;
     end;
 end;
 
 
 if not foundIt then
+    
     sendSMS(conf_num, caller_id, 'WARNING', string.format("caller[%s] not belong conference[%s]", caller_id, conf_num));
     session:hangup();
 end;
