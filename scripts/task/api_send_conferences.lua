fedebacccf732f18ed9330c21ffca90473d943b8
xiuheng chen <chenxiuheng@gmail.com>
2015-07-03 18:27:37 +0800

    会议列表分页显示

--- scripts/task/api_send_conferences.lua
+++ scripts/task/api_send_conferences.lua
@@ -3,20 +3,37 @@ require('conference.conferenceService');
 function api_send_conferences (from_user, to_user)
     local conferences = getMyConferences(to_user);
 
-    local buf = newStringBuilder('conference-list');
-    for i, info in ipairs(conferences) do
-        buf.append('\n');
-
-        buf.append(info['conference']).append(';');
-        buf.append(info['name']).append(';');
-        buf.append(info['creator']).append(';');
-        buf.append(info['creator_name']).append(';');
-        buf.append(info['created']).append(';');
-        buf.append(info['num_is_in']).append('/').append(info['num_member']).append(';');
+    local totalCount = #conferences; -- 求table长度, 好变态的语法!
+    if 0 == totalCount then
+        sendSMS(from_user, to_user, 'conference-list\n');
+        return;        
     end;
-    buf.append('\n');
 
-    sendSMS(from_user, to_user, buf.toString());
+    local index = 0;
+    local pageNo = 0;
+    local pageSize = 6;
+    local numPage = math.ceil(totalCount / pageSize);
+    while pageNo < numPage do
+        index = pageNo * pageSize;
+
+        local buf = newStringBuilder('conference-list\n');
+        buf.append(pageNo + 1).append('/').append(numPage).append('/').append(totalCount).append('\n');
+        while index < (pageNo + 1) * pageSize and index < totalCount do
+            local info = conferences[index + 1];
+            buf.append(info['conference']).append(';');
+            buf.append(info['name']).append(';');
+            buf.append(info['creator']).append(';');
+            buf.append(info['creator_name']).append(';');
+            buf.append(info['created']).append(';');
+            buf.append(info['num_is_in']).append('/').append(info['num_member']).append(';');
+            buf.append('\n');
+
+            index = index + 1;
+        end;
+        sendSMS(from_user, to_user, buf.toString());
+
+        pageNo = pageNo + 1;
+    end;
 end;
 
 --  execute if has argv
